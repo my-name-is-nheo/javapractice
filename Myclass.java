a0dc7ad42c7e088a984fbcd65ca82b1e97b18450
Your Name <you@example.com>
2020-05-29 14:40:08 -0400

    progress

--- Myclass.java
+++ Myclass.java
@@ -1,6 +1,19 @@
+// running javac SomeFile.java will compile the code. If there are no errors in the code, the command prompt will take you the next line. 
+// also created a .class file 
+// every line of code that runs in Java must be inside a class. A class should always start with an uppercase first letter. 
 public class MyClass {
   public static void main(String[] args) {
     System.out.println("Hello World");
   }
 }
-// practice
+/*
+The name of the java file must match the class name. 
+  public class MyClass{}
+  public class MyClass{}
+  public class MyClass{}
+  public class MyClass{}
+  public class MyClass{}
+  public class MyClass{}
+  public class MyClass{}
+  public class MyClass{}
+*/
