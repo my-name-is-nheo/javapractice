cec694db758bf8c0f6dc16fd7071e962875de362
Your Name <you@example.com>
2020-05-29 14:54:07 -0400

    progress

--- Myclass.java
+++ Myclass.java
@@ -16,4 +16,45 @@ The name of the java file must match the class name.
   public class MyClass{}
   public class MyClass{}
   public class MyClass{}
+
+The main method
+- require and will be seen  in every java program. 
+- any code inside the main() method will be executed
+- every java program has a class name which must match the filename, and that every program must contain the main() method
+public class MyClass{
+  public static void (String[] args){
+    System.out.println("Hello World")
+  }
+}
+public class MyClass{
+  public static void ( String[] args){
+    System.out.println("Hello World")
+  }
+}
+public class MyClass{
+  public static void (String[] args){
+    System.out.println("Hellow World")
+  }
+}
+public class MyClass(){
+  public static void (String[] args){
+    System.out.println("Hello World")
+  }
+}
+public class MyClass {
+  public static void (String[] args){
+    System.out.println("Hello Word")
+  }
+}
+public class MyClass {
+  public static void (String[] args){
+    System.out.println("hello World")
+  }
+}
+
+public class MyClass{
+  public static void (String[] args){
+    System.out.println("Hello World")
+  }
+}
 */
